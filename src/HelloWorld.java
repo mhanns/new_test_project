54b16aa917a9448ef745854cdfd89c18bbf96e5a
Hanns Jr <mahannsjr.2017@mymail.jcsu.edu>
2018-01-24 13:17:11 -0500

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+//Maurice Hanns CSC 242 01/24/2018 
+
+public class HelloWorld {
+
+	public static void main(String [] args){
+		
+
+		char I = 'H',am = 'e',back = 'l', to = 'l', printing = 'o';  		//My unique Hello
+		char out = 'W', Hello = 'o', World = 'r', once = 'l', again = 'd'; 	//My unique World
+		
+		System.out.println("\"" + I  + am + back  + to + printing + ", " + out + Hello + World + once + again + "\"");
+	}
+}
